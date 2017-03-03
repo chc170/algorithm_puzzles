c13b96944748d9948e21226fbadbdf96d4432339
Chih-Chuan Chan <dincoco@gmail.com>
2017-03-03 17:04:36 -0500

    version 3

--- question9.py
+++ question9.py
@@ -1,9 +1,87 @@
 from math import pow
 from time import time
+from collections import defaultdict
 
 cache = {}
 
 def answer(g):
+
+    rows = len(g)
+    cols = len(g[0])
+    new_state = ''.join(['x'] * (cols+2))
+    state = {
+        'xxx' : {new_state : 1},
+    }
+    t1 = time()
+    for i in range(rows):
+        for j in range(cols):
+            state = find_answer(state, g, i, j)
+            n_states = 0
+            for _, val in state.items():
+                n_states += len(val)
+            output = '# of keys: {}. # of states: {}'.format(len(state),
+                                                             n_states)
+            print(output)
+
+    print('Run time: {}'.format(time()-t1))
+    #print(state)
+    total = 0
+    for _, state in state.items():
+        for _, count in state.items():
+            total += count
+    return total
+
+def find_answer(state, g, i, j):
+
+    possible_fill_ins = {
+        False : [
+            [[1, 1], [0, 0]],
+            [[1, 0], [1, 0]],
+            [[1, 0], [0, 1]],
+            [[0, 1], [1, 0]],
+            [[0, 1], [0, 1]],
+            [[0, 0], [1, 1]],
+            [[1, 1], [1, 0]],
+            [[1, 1], [0, 1]],
+            [[1, 0], [1, 1]],
+            [[0, 1], [1, 1]],
+            [[1, 1], [1, 1]],
+            [[0, 0], [0, 0]],
+        ],
+        True : [
+            [[0, 0], [0, 1]],
+            [[0, 0], [1, 0]],
+            [[0, 1], [0, 0]],
+            [[1, 0], [0, 0]],
+        ]
+    }
+    #print(state)
+    nxt_state = {}
+    for fill in possible_fill_ins[g[i][j]]:
+        if i == 0 and j == 0:
+            key = 'xxx'
+        elif i == 0:
+            key = str(fill[1][0]) + str(fill[0][0]) + 'x'
+        elif j == 0:
+            key = 'x' + str(fill[0][0]) + str(fill[0][1])
+        else:
+            key = str(fill[1][0]) + str(fill[0][0]) + str(fill[0][1])
+
+        for prev, count in state.get(key, {}).items():
+            part = str(fill[1][0]) + str(fill[1][1]) + str(fill[0][1])
+            new_state = prev[:j] + part + prev[j+3:]
+            if j == (len(g[0]) - 1):
+                new_state = 'x' + new_state[:-1]
+
+            nxt_j = 0 if (j == len(g[0])-1) else j+1
+            new_key = new_state[nxt_j:nxt_j+3]
+            nxt_state[new_key] = nxt_state.get(new_key, {})
+            nxt_state[new_key][new_state] = nxt_state[new_key].get(new_state, 0)
+            nxt_state[new_key][new_state] += count
+
+    return nxt_state
+
+def answer_slow(g):
     global cache
     cache = {}
     ######
@@ -11,7 +89,7 @@ def answer(g):
     cols = len(g[0])
     check = [[None for _ in range(cols + 1)] for _ in range(rows + 1)]
     t1 = time()
-    count = find_answer(check, g, 0, 0)
+    count = find_answer_slow(check, g, 0, 0)
     print('Run time: {}'.format(time()-t1))
     return count
     print('Result1: {}.'.format(count))
@@ -37,19 +115,19 @@ def answer(g):
     #print(time()-t1)
     #print('Result2: {}.'.format(count))
 
-    
-def find_answer_(state, g, i, j):
+
+def find_answer_slow(state, g, i, j):
 
     if i == len(g):
         return 1
-    
+
     global cache
     key = None
 
 
     nxt_j = j + 1 if (j+1) < len(g[0]) else 0
     nxt_i = i + 1 if nxt_j == 0 else i
-    
+
     possible_fill_ins = {
         False : [
             [[1, 1], [0, 0]],
@@ -72,7 +150,7 @@ def find_answer_(state, g, i, j):
             [[1, 0], [0, 0]],
         ]
     }
-    
+
     count = 0
     for fill in possible_fill_ins[g[i][j]]:
         if i == 0 and j == 0:
@@ -81,7 +159,7 @@ def find_answer_(state, g, i, j):
         if i == 0:
             state[i][j+1] = fill[0][1]
 
-        if j == 0: 
+        if j == 0:
             state[i+1][j] = fill[1][0]
 
         if state[i][j] != fill[0][0] or \
@@ -89,7 +167,7 @@ def find_answer_(state, g, i, j):
            state[i+1][j] != fill[1][0]:
             continue
 
-        state[i+1][j+1] = fill[1][1] 
+        state[i+1][j+1] = fill[1][1]
 
         key = ''
         for idx in range(len(state[0])):
@@ -101,7 +179,7 @@ def find_answer_(state, g, i, j):
         if key in cache:
             cnt = cache[key]
         else:
-            cnt = find_answer(state, g, nxt_i, nxt_j)
+            cnt = find_answer_slow(state, g, nxt_i, nxt_j)
             cache[key] = cnt
         count += cnt
     return count
@@ -119,7 +197,7 @@ def next_state(state):
 g = [
     [True, False, True],
     [False, True, False],
-    [True, False, True] 
+    [True, False, True]
 ]
 
 print('9 Expected: 4.  Result: {}.'.format(answer(g)))
@@ -168,6 +246,4 @@ g = [
 [True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True],
 [True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True, True, False, True, False, False, True, True, True, True, True],
 ]
-print('450 Expected: ?.   Result: {}.'.format(answer(g)))
-
-
+#print('450 Expected: ?.   Result: {}.'.format(answer(g)))
